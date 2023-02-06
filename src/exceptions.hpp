170b5e0932ad2016725c67ce50257925545c1510
Maksym Bodnar <macsimbodnar@gmail.com>
2023-02-07 00:06:57 +0100

    update pixello

--- src/exceptions.hpp
+++ src/exceptions.hpp
@@ -7,10 +7,3 @@ class FAN_exception : public pixello_exception
 public:
   FAN_exception(std::string msg) : pixello_exception(std::move(msg)) {}
 };
-
-
-class input_exception : public pixello_exception
-{
-public:
-  input_exception(std::string msg) : pixello_exception(std::move(msg)) {}
-};
