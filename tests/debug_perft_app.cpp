cef10d998ea752582340623a80b9de60b5cdb0f4
Maksym Bodnar <macsimbodnar@gmail.com>
2025-05-12 12:35:02 +0200

    Fix teh perft debug app

--- tests/debug_perft_app.cpp
+++ tests/debug_perft_app.cpp
@@ -36,12 +36,32 @@ std::vector<std::string> split(std::string s, std::string delimiter)
 }
 
 
+enum mini_promotion_t
+{
+  TO_NOPE,
+  TO_Q,
+  TO_N,
+  TO_R,
+  TO_B
+};
+
+
 struct mini_move_t
 {
   index_t from;
   index_t to;
-  piece_t promotion;
+  mini_promotion_t promotion;
 };
+
+
+bool is_same_promotion(mini_promotion_t p1, piece_t p2)
+{
+  if (p1 == TO_Q && (p2 != W_QUEEN && p2 != B_QUEEN)) { return false; }
+  if (p1 == TO_N && (p2 != W_KNIGHT && p2 != B_KNIGHT)) { return false; }
+  if (p1 == TO_R && (p2 != W_ROOK && p2 != B_ROOK)) { return false; }
+  if (p1 == TO_B && (p2 != W_BISHOP && p2 != B_BISHOP)) { return false; }
+  return true;
+}
 
 
 mini_move_t algebraic_to_mini_move(const std::string& p)
@@ -75,34 +95,29 @@ mini_move_t algebraic_to_mini_move(const std::string& p)
   mini_move_t move;
   move.from = from;
   move.to = to;
-  move.promotion = W_PAWN;
+  move.promotion = TO_NOPE;
   // TODO: handle the of promotion
   if (p.length() == 5) {
     // Handle promotion
     switch (p[4]) {
       case 'Q':
-        move.promotion = W_QUEEN;
-        break;
       case 'q':
-        move.promotion = B_QUEEN;
+        move.promotion = TO_Q;
         break;
+
       case 'N':
-        move.promotion = W_KNIGHT;
-        break;
       case 'n':
-        move.promotion = B_KNIGHT;
+        move.promotion = TO_N;
         break;
+
       case 'R':
-        move.promotion = W_ROOK;
-        break;
       case 'r':
-        move.promotion = B_ROOK;
+        move.promotion = TO_R;
         break;
+
       case 'B':
-        move.promotion = W_BISHOP;
-        break;
       case 'b':
-        move.promotion = B_BISHOP;
+        move.promotion = TO_B;
         break;
 
       default:
@@ -117,7 +132,7 @@ mini_move_t algebraic_to_mini_move(const std::string& p)
 
 std::string promotion_to_string(const move_t& move)
 {
-  switch (MOVE_CAPTURE(move)) {
+  switch (MOVE_PROMOTED(move)) {
     case B_QUEEN:
     case W_QUEEN:
       return "q";
@@ -136,7 +151,7 @@ std::string promotion_to_string(const move_t& move)
       break;
 
     default:
-      assert(false);
+      return "";
       break;
   }
 
@@ -145,33 +160,54 @@ std::string promotion_to_string(const move_t& move)
 }
 
 
-uint64_t perft(int depth, board_t* board, global_state_t* globals)
+size_t debug_generate_legal_moves(game_t* game, move_t moves[])
 {
+  size_t count = 0;
+  move_t all_moves[MAX_MOVES];
+  const size_t all_moves_count =
+      generate_moves(&game->tables, &game->board, all_moves);
+
+  assert(all_moves_count < MAX_MOVES);
+
+  for (size_t i = 0; i < all_moves_count; ++i) {
+    if (is_move_legal(game, all_moves[i])) { moves[count++] = all_moves[i]; }
+  }
+
+  return count;
+}
+
+
+uint64_t perft(int depth, game_t* game)
+{
+  assert(game != nullptr);
+
   uint64_t nodes = 0;
 
   if (depth == 0) { return 1; }
 
   move_t moves[270];
-  const size_t moves_count = generate_legal_moves(board, globals, moves);
+  const size_t moves_count = generate_moves(&game->tables, &game->board, moves);
   assert(moves_count <= 270);
   // node_stats += get_moves_stats(moves);
 
   for (size_t i = 0; i < moves_count; ++i) {
-    make_move(&moves[i], board, globals);
-    nodes += perft(depth - 1, board, globals);
-    unmake_move(board, globals);
+    if (make_move(game, moves[i])) {
+      nodes += perft(depth - 1, game);
+      unmake_move(game);
+    }
   }
 
   return nodes;
 }
 
-static board_t board = {};
-static global_state_t globals = {};
 
+static game_t g_game;
 
 // "$depth" "$fen" "$moves"
 int main(int argc, char* argv[])
 {
+  initialize_const_data(&g_game.tables);
+
   if (argc < 3) {
     std::cout << "No arguments. Use \"$depth\" \"$fen\" \"$moves\"\n";
     return 1;
@@ -192,10 +228,10 @@ int main(int argc, char* argv[])
     }
   }
 
-  init_board(fen, &board, &globals);
+  load_FEN(fen, &g_game.board, &g_game.history);
 
   move_t moves[270];
-  size_t moves_count = generate_legal_moves(&board, &globals, moves);
+  size_t moves_count = debug_generate_legal_moves(&g_game, moves);
 
   // Navigate the moves
   while (!moves_to_apply.empty()) {
@@ -204,15 +240,17 @@ int main(int argc, char* argv[])
 
     bool found = false;
     for (size_t i = 0; i < moves_count; ++i) {
-      const move_t& move = moves[i];
+      const move_t move = moves[i];
+      const unpacked_move_t um(move);
 
-      if (move.from == mini_move.from && move.to == mini_move.to &&
-          move.promoted_to == mini_move.promotion) {
+      if (um.from == mini_move.from && um.to == mini_move.to &&
+          is_same_promotion(mini_move.promotion, um.promoted_to)) {
         found = true;
-        bool move_happened = make_move(&move, &board, &globals);
+
+        bool move_happened = make_move(&g_game, move);
         (void)move_happened;
         assert(move_happened);
-        moves_count = generate_legal_moves(&board, &globals, moves);
+        moves_count = debug_generate_legal_moves(&g_game, moves);
         break;
       }
     }
@@ -227,27 +265,26 @@ int main(int argc, char* argv[])
     if (depth > 1) {
       tot_nodes = 0;
       for (size_t i = 0; i < moves_count; ++i) {
-        const move_t& move = moves[i];
+        const move_t move = moves[i];
 
-        make_move(&move, &board, &globals);
-        uint64_t num_of_nodes = perft(depth - 1, &board, &globals);
-        unmake_move(&board, &globals);
+        bool move_happened = make_move(&g_game, move);
+        (void)move_happened;
+        assert(move_happened);
+
+        uint64_t num_of_nodes = perft(depth - 1, &g_game);
+        unmake_move(&g_game);
         tot_nodes += num_of_nodes;
 
-        std::cout << index_to_string_coordinates(move.from)
-                  << index_to_string_coordinates(move.to)
-                  << (move.promoted_to != TO_NONE ? promotion_to_string(move)
-                                                  : "")
+        std::cout << index_to_str(MOVE_FROM(move))
+                  << index_to_str(MOVE_TO(move)) << promotion_to_string(move)
                   << " " << num_of_nodes << std::endl;
       }
     } else {
       tot_nodes = moves_count;
       for (size_t i = 0; i < moves_count; ++i) {
         const move_t& move = moves[i];
-        std::cout << index_to_string_coordinates(move.from)
-                  << index_to_string_coordinates(move.to)
-                  << (move.promoted_to != TO_NONE ? promotion_to_string(move)
-                                                  : "")
+        std::cout << index_to_str(MOVE_FROM(move))
+                  << index_to_str(MOVE_TO(move)) << promotion_to_string(move)
                   << " " << 1 << std::endl;
       }
     }
