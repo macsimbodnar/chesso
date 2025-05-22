7a07e602cff9321fb0f7b7d68320e1c3e4bc82bc
Maksym Bodnar <macsimbodnar@gmail.com>
2025-05-22 11:29:35 +0200

    Debuging search

--- TODO.md
+++ TODO.md
@@ -38,3 +38,16 @@
 ## Bugs
 
 * In the evaluate move function handle the en-passant
+* The engine stack in this search:
+  >Chesso Bitboard(1): position startpos moves g1f3 d7d5 d2d4 g8f6 c2c4 e7e6 b1c3 c7c5 c4d5 f6d5 e2e4 d5c3 b2c3 c5d4 c3d4 f8b4 c1d2 b4d2 d1d2 e8g8 a1d1 b7b6 f1e2 b8d7 e4e5 c8b7 e1g1 a8c8 d1c1 c8c1 d2c1 f8e8 f1e1 d7f8 c1b2 f8g6 e2b5 e8e7 e1c1 b7f3 g2f3 f7f6 b5a6 e7c7 c1c7 d8c7 a6d3 g6h4 d3e2 f6e5 d4e5 h4g6 f3f4 g6f4 e2f3 g7g6 g1f1 c7c5 f3g2 f4d3 b2d2 d3e5 f1g1 b6b5 h2h4 g8g7 g1h2 a7a5 h2h1 e5c4 d2d7 g7h6 d7e6 c5f2 e6e7 f2f5 h1g1 a5a4 g1h1 a4a3 h1h2 f5f4 h2h3 c4e3 e7e4 e3g2 e4g2 b5b4 g2e2 f4f5 h3h2 f5f6 h2h3 h6g7 h3g4 h7h5 g4h3 b4b3 a2b3 f6c3 h3g2 c3b2 g2f1 b2e2 f1e2 a3a2 b3b4 a2a1q e2d3 a1f6 d3c4 f6h4 c4b3 h4g5 b3a3 h5h4 a3a4 h4h3 a4a3 h3h2 a3b3 h2h1q
+  >Chesso Bitboard(1): isready
+  <Chesso Bitboard(1): readyok
+  >Chesso Bitboard(1): go depth 9
+  <Chesso Bitboard(1): info score cp -2022 time 0 depth 1 nodes 13 pv b3b2
+  <Chesso Bitboard(1): info score cp -2029 time 0 depth 2 nodes 450 pv b3b2 g5f6 b2b3
+  <Chesso Bitboard(1): info score cp -2053 time 1 depth 3 nodes 3341 pv b3a3 g5e3 a3b2 h1c1 b2a2
+  <Chesso Bitboard(1): info score mate -4 time 7 depth 4 nodes 35536 pv b3b2 g5d2 b2b3 h1d5 b3a3 d5a2
+  <Chesso Bitboard(1): info score mate -4 time 19 depth 5 nodes 100188 pv b3b2 h1d5 b4b5 g5d2 b2b1 d5a2
+  <Chesso Bitboard(1): info score mate -4 time 133 depth 6 nodes 669582 pv b3b2 h1d5 b4b5 g5d2 b2a3 d5a2
+  <Chesso Bitboard(1): info score mate -4 time 1629 depth 7 nodes 7456262 pv b3b2 h1d5 b2a3 d5c4 a3a4 c4a2
+  <Chesso Bitboard(1): info score mate -4 time 35350 depth 8 nodes 182277694 pv b3b2 h1d5 b4b5 d5c4 b2b1 g5c1
--- src/search.cpp
+++ src/search.cpp
@@ -13,10 +13,10 @@
 #define MATE_MIN 48000
 #define DRAW_SCORE 0
 
-// static constexpr int MIN = std::numeric_limits<int>::min() + 100;
-static constexpr int MIN = -2000000000;
-// static constexpr int MAX = std::numeric_limits<int>::max() - 100;
-static constexpr int MAX = 2000000000;
+static constexpr int MIN = std::numeric_limits<int>::min() + 100;
+// static constexpr int MIN = -2000000000;
+static constexpr int MAX = std::numeric_limits<int>::max() - 100;
+// static constexpr int MAX = 2000000000;
 // static constexpr int NO_SCORE = MAX + 42;
 
 #define NULL_MOVE_REDUCTION 2
@@ -28,8 +28,7 @@ int quiescence(int alpha,
                int beta,
                size_t ply,
                game_t* game,
-               search_state_t* state,
-               index_t last_to)
+               search_state_t* state)
 {
   assert(game != nullptr);
 
@@ -37,13 +36,6 @@ int quiescence(int alpha,
                         evaluate(&game->tables, &game->board);
 
   state->explored_nodes++;
-
-  // DELTA PRUNE:
-  // if (stand_pat + get_max_gain() <= alpha) {
-  if (stand_pat + 200 <= alpha) {
-    // At this point no capture can improve te score so we just return
-    return alpha;
-  }
 
   if (stand_pat >= beta) { return beta; }
   if (alpha < stand_pat) { alpha = stand_pat; }
@@ -58,14 +50,10 @@ int quiescence(int alpha,
   order_captures(&game->board, moves, n);
 
   for (size_t i = 0; i < n; ++i) {
-    // We consider only the captures that recapture the last capture
     if (!MOVE_CAPTURE(moves[i])) { continue; }
-    if (last_to != INVALID_INDEX && MOVE_TO(moves[i]) != last_to) { continue; }
-
     if (!make_move(game, moves[i])) { continue; }
 
-    const int s =
-        -quiescence(-beta, -alpha, ply + 1, game, state, MOVE_TO(moves[i]));
+    const int s = -quiescence(-beta, -alpha, ply + 1, game, state);
 
     unmake_move(game);
 
@@ -82,9 +70,7 @@ int negamax(int alpha0,
             int depth,
             size_t ply,
             game_t* game,
-            search_state_t* state,
-            bool zero_window,
-            index_t last_to)
+            search_state_t* state)
 {
   assert(game != nullptr);
   assert(state != nullptr);
@@ -93,17 +79,17 @@ int negamax(int alpha0,
   int alpha = alpha0;
 
   // Reuse TT entry if found
-  const tt_entry_t* tt_entry = tt_get_entry(state->tt, &game->board);
-  if (ply > 0 && tt_entry != nullptr && tt_entry->depth >= depth) {
-    if (tt_entry->type == TT_PV_NODE) {
-      state->best_move = tt_entry->best_move;
-      return tt_entry->score;
-    } else if (tt_entry->type == TT_BETA_NODE && tt_entry->score >= beta) {
-      return tt_entry->score;
-    } else if (tt_entry->type == TT_ALPHA_NODE && tt_entry->score <= alpha) {
-      return tt_entry->score;
-    }
-  }
+  // const tt_entry_t* tt_entry = tt_get_entry(state->tt, &game->board);
+  // if (ply > 0 && tt_entry != nullptr && tt_entry->depth >= depth) {
+  //   if (tt_entry->type == TT_PV_NODE) {
+  //     state->best_move = tt_entry->best_move;
+  //     return tt_entry->score;
+  //   } else if (tt_entry->type == TT_BETA_NODE && tt_entry->score >= beta) {
+  //     return tt_entry->score;
+  //   } else if (tt_entry->type == TT_ALPHA_NODE && tt_entry->score <= alpha) {
+  //     return tt_entry->score;
+  //   }
+  // }
 
   // Check for repetitions
   if (ply > 0 && is_position_repeated(&game->repetitions, game->board.hash)) {
@@ -114,20 +100,6 @@ int negamax(int alpha0,
 
   if (is_in_check) { ++depth; }
 
-  // Razoring
-  // if (!is_in_check && depth == 1) {
-  //   int stand_pat =
-  //       (game->board.active_color == WHITE ? +1 : -1) *
-  //       evaluate(&game->board);
-  //   const int razor_margin = get_margin_value();
-  //   // const int razor_margin = 200;
-
-  //   if (stand_pat + razor_margin < alpha) {
-  //     // No quiet move can possibly raise the score above alpha
-  //     return stand_pat;
-  //   }
-  // }
-
   // Time management
   if ((state->explored_nodes % 1000) && *state->stop) {
     return (game->board.active_color == WHITE ? 1 : -1) *
@@ -135,108 +107,33 @@ int negamax(int alpha0,
   }
 
   if (depth < 1 || ply > (MAX_PLY - 2)) {
-    return quiescence(alpha, beta, ply, game, state, last_to);
+    return quiescence(alpha, beta, ply, game, state);
   }
 
   state->explored_nodes += 1;
   state->pv.pv_length[ply] = ply;
   node_type_t type = TT_ALPHA_NODE;
 
+  int legal_moves_counter = 0;
   move_t moves[MAX_MOVES];
   const size_t moves_count = generate_moves(&game->tables, &game->board, moves);
-
-  // TODO: Fix this. For now it's down below
-  // if (moves_count == 0) { return is_in_check ? -(MATE_MAX - ply) :
-  // DRAW_SCORE; }
 
   order_moves(&game->board, state, ply, moves_count, moves);
 
   // initialize best move just in case. TODO: this might be illegal move
   move_t best_move = moves[0];
 
-  // Null move pruning
-  if (depth > NULL_MOVE_REDUCTION + 1 && !is_in_check && !zero_window) {
-    swap_side(game);
-    const index_t en_passant = game->board.en_passant;
-    set_en_passant(game, INVALID_INDEX);
-
-    const int probe_score =
-        -negamax(-beta, -beta + 1, depth - NULL_MOVE_REDUCTION - 1, ply + 1,
-                 game, state, true, INVALID_INDEX);
-
-    if (probe_score >= beta) {
-      // Verified null move pruning. Going 1 ply deeper
-      const int verify_score =
-          -negamax(-beta, -beta + 1, depth - NULL_MOVE_REDUCTION, ply + 1, game,
-                   state, true, INVALID_INDEX);
-
-      if (verify_score >= beta) {
-        swap_side(game);
-        set_en_passant(game, en_passant);
-
-        //  Now it's safe to cut off
-        return beta;
-      }
-    }
-
-    swap_side(game);
-    set_en_passant(game, en_passant);
-  }
-
-  int legal_moves_counter = 0;
   for (size_t i = 0; i < moves_count; ++i) {
-    if (is_capturing_king(&game->board, moves[i])) {
-      // TODO: Does this check even make any sense at all?
-      state->best_move = moves[i];
-
-      state->pv.pv_table[ply][ply] = moves[i];
-      state->pv.pv_length[ply] = ply + 1;
-
-      assert(false);
-
-      return MATE_MAX - ply;
-    }
-
     if (!make_move(game, moves[i])) { continue; }
+
+    const bool is_capture = MOVE_CAPTURE(moves[i]);
+    const bool is_check_move = is_check(game);
 
     state->pv.pv_length[ply + 1] = ply + 1;
 
     // best_move = moves[i];
     legal_moves_counter++;
-
-    const bool is_capture = MOVE_CAPTURE(moves[i]);
-    const bool is_check_move = is_check(game);
-
-    // Decide depth reduction R
-    int R = 0;
-    if (!zero_window && !is_capture && !is_check_move &&
-        i >= LMR_WHEN_START_IN_THE_LIST && depth >= LMR_START_AT_DEPTH) {
-      R = 1;  // TODO: Compute dynamically
-    }
-
-    // Decide if this is “first full” move in PVS
-    const bool pvs = (!zero_window && i > 0);
-
-    // Compute the search window
-    const int low = pvs ? -(alpha + 1) : -beta;
-    const int high = pvs ? -alpha : -alpha;
-    const int new_depth = depth - 1 - R;
-    const bool zw = R > 0 || pvs;
-
-    // Probe search
-    int score = -negamax(low, high, new_depth, ply + 1, game, state, zw,
-                         MOVE_TO(moves[i]));
-
-    // If the probe suggests it might raise alpha, do a full‐window re‐search
-    const bool LMR_probe_beat_alpha = (R > 0 && score > alpha);
-    const bool PVS_in_ab_interval =
-        (R == 0 && pvs && score > alpha && score < beta);
-
-
-    if (LMR_probe_beat_alpha || PVS_in_ab_interval) {
-      score = -negamax(-beta, -alpha, depth - 1, ply + 1, game, state, false,
-                       MOVE_TO(moves[i]));
-    }
+    const int score = -negamax(-beta, -alpha, depth - 1, ply + 1, game, state);
 
     unmake_move(game);
 
@@ -253,7 +150,8 @@ int negamax(int alpha0,
         state->killer_moves[0][ply] = moves[i];
 
         const int bonus = depth * depth;
-        state->history_moves[MOVE_PIECE(moves[i])][MOVE_TO(moves[i])] += bonus;
+        state->history_moves[MOVE_PIECE(moves[i])][MOVE_TO(moves[i])] +=
+        bonus;
       }
 
       break;
@@ -282,7 +180,9 @@ int negamax(int alpha0,
   }
 
   // Store the node in TT
-  tt_store_entry(state->tt, &game->board, depth, best_so_far, type, best_move);
+  (void)type;
+  // tt_store_entry(state->tt, &game->board, depth, best_so_far, type,
+  // best_move);
 
   state->best_move = best_move;
   return (best_so_far != MIN) ? best_so_far : (alpha0 - 1);
@@ -298,8 +198,7 @@ search_t search(int depth, game_t* game, search_state_t* state)
 
   search_t search_result = {};
 
-  const int score =
-      negamax(MIN, MAX, depth, 0, game, state, false, INVALID_INDEX);
+  const int score = negamax(MIN, MAX, depth, 0, game, state);
 
   assert(state->pv.pv_length[0] > 0);
 
